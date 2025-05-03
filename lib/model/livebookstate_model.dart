268928bfe043fd129481c2f854fd3c58dacd0068
rafeefdev <rafeef.dev03@gmail.com>
2025-05-03 22:05:50 +0700

    separate-livebookstate-to-a-file

---
+++ lib/model/livebookstate_model.dart
@@ -0,0 +1,14 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:google_book/model/book_model.dart';
+
+class LiveBookState extends Equatable {
+  final String status;
+  final String message;
+  final List<Book> data;
+
+  const LiveBookState(this.status, this.message, this.data);
+
+  @override
+  List<Object?> get props => [status, message, data];
+}
