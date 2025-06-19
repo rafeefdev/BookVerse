b1fdf7db11fafcb6c1fb010d828155c306151131
rafeefdev <rafeef.dev03@gmail.com>
2025-06-19 10:45:53 +0700

    change with new android folder

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,21 +5,10 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-// Definisikan fungsi untuk membaca versi dari Flutter
-fun flutterVersionCode(): String {
-    val flutterVersionCode = project.findProperty("flutter.versionCode")
-    return flutterVersionCode?.toString() ?: "1"
-}
-
-fun flutterVersionName(): String {
-    val flutterVersionName = project.findProperty("flutter.versionName")
-    return flutterVersionName?.toString() ?: "1.0.0"
-}
-
 android {
-    namespace = "com.bookverse.app"
+    namespace = "com.example.bookverse"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -32,14 +21,13 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.bookverse.app"
-        ndkVersion = "27.0.12077973"
+        applicationId = "com.example.bookverse"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutterVersionCode().toInt()
-        versionName = flutterVersionName()
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
