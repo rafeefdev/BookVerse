7b5cdad8109f340d7559bcbe8c1885b19aaa0f0b
rafeefdev <rafeef.dev03@gmail.com>
2025-06-20 14:31:10 +0700

    remove unnecessary import

--- lib/view/pages/settings_page.dart
+++ lib/view/pages/settings_page.dart
@@ -1,6 +1,5 @@
 import 'package:book_verse/provider/thememode_provider.dart';
 import 'package:book_verse/shared/themes_extension.dart';
-import 'package:book_verse/view/components/settingsswitch_component.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
