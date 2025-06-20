0c72199e06e8ce6755bc0dc3fa58bd94d0b18b78
rafeefdev <rafeef.dev03@gmail.com>
2025-06-20 17:13:21 +0700

    init supabase and change app theming method

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:book_verse/provider/thememode_provider.dart';
 import 'package:book_verse/view/onboarding_router.dart';
+import 'package:book_verse/view/pages/authentication_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:book_verse/shared/app_theme.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
-  await dotenv.load(); //load .env file
+  WidgetsFlutterBinding.ensureInitialized();
+  //load .env file
+  await dotenv.load();
+  
+  //initialize supabase
+  await Supabase.initialize(
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    url: dotenv.env['SUPABASE_URL']!,
+  );
+
   runApp(ProviderScope(child: MyApp()));
 }
 
@@ -15,7 +26,8 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final ThemeMode appThemeMode = ref.watch(thememodeProviderProvider).value ?? ThemeMode.system;
+    final ThemeMode appThemeMode =
+        ref.watch(thememodeProviderProvider).value ?? ThemeMode.system;
 
     return MaterialApp(
       title: 'book_verse',
@@ -23,6 +35,7 @@ class MyApp extends ConsumerWidget {
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: appThemeMode,
+      // home: AuthenticationPage(),
       home: const OnboardingRouter(),
     );
   }
