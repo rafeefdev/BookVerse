3caa933b734789764f3f30a6d3d4b2f0d85c2ea8
rafeefdev <rafeef.dev03@gmail.com>
2025-10-19 20:00:49 +0700

    add multiplatfrom sqflite support, change savedbookpage data handling

--- lib/core/services/sqflite_service.dart
+++ lib/core/services/sqflite_service.dart
@@ -1,5 +1,6 @@
+import 'dart:io';
 import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 class SqfliteService {
   static final SqfliteService instance = SqfliteService._init();
@@ -7,20 +8,56 @@ class SqfliteService {
 
   SqfliteService._init();
 
+  /// Getter database
   Future<Database> get database async {
     if (_database != null) return _database!;
     _database = await _initDatabase('bookverse.db');
     return _database!;
   }
 
+  /// Inisialisasi database sesuai platform
   Future<Database> _initDatabase(String dbName) async {
-    final path = await getDatabasesPath();
-    final fullPath = join(path, dbName);
-    return openDatabase(fullPath, version: 1);
+    if (Platform.isAndroid || Platform.isIOS) {
+      // Mobile pakai sqflite biasa
+      final dbPath = await getDatabasesPath();
+      final fullPath = join(dbPath, dbName);
+      return await openDatabase(fullPath, version: 1, onCreate: _onCreate);
+    } else {
+      // Desktop pakai sqflite_common_ffi
+      sqfliteFfiInit();
+      databaseFactory = databaseFactoryFfi;
+
+      final dbPath = await databaseFactory.getDatabasesPath();
+      final fullPath = join(dbPath, dbName);
+      return await databaseFactory.openDatabase(
+        fullPath,
+        options: OpenDatabaseOptions(version: 1, onCreate: _onCreate),
+      );
+    }
   }
 
+  /// Buat tabel awal
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS bookmarks (
+        id TEXT PRIMARY KEY,
+        title TEXT,
+        authors TEXT,
+        description TEXT,
+        thumbnail TEXT,
+        publishedDate TEXT,
+        pageCount INTEGER,
+        categories TEXT,
+        publisher TEXT,
+        subTitle TEXT,
+        isFavorite INTEGER
+      )
+    ''');
+  }
+
+  /// Mengecek apakah tabel ada
   Future<bool> isTableExists(String tableName) async {
-    final db = await SqfliteService.instance.database;
+    final db = await database;
     final result = await db.rawQuery(
       '''
         SELECT 
